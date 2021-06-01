52b89eb013d39038f99306871cadd95c494b6bf1
Twan van Noorloos <twan.v.n@outlook.com>
2021-06-01 10:37:49 +0200

    added medal activity

---
+++ app/src/main/java/nl/avans/ti/medalAvtivity.java
@@ -0,0 +1,14 @@
+package nl.avans.ti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medalAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medal_avtivity);
+    }
+}
