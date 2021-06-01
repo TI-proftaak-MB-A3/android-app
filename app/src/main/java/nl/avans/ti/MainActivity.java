f7636a6ee157bab6142b3b98e24b604eb10d96eb
Ewout <EwoutBaars@live.nl>
2021-06-01 13:06:05 +0200

    Made a simple home screen, with a placeholder button for opening the medals view

--- app/src/main/java/nl/avans/ti/MainActivity.java
+++ app/src/main/java/nl/avans/ti/MainActivity.java
@@ -1,23 +1,57 @@
 package nl.avans.ti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import nl.avans.ti.Medal.MedalActivity;
-import nl.avans.ti.Medal.MedalListAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+
+    EditText editText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextNumberSigned);
+        Button button = findViewById(R.id.button);
+
+
     }
 
-    public void openMedals(View view) {
+
+    public void onButtonPressed(View view)
+    {
+        String enteredCode = editText.getText().toString();
+
+        if (enteredCode.length() < 4)
+        {
+            Toast.makeText(view.getContext(), "de code moet 4 cijfers lang zijn", Toast.LENGTH_LONG).show();
+            Log.d(String.valueOf(view.getTag()), "value entered didn't meet the requirments");
+        }
+
+
+        //todo validate code and start activity
+        Intent intent = new Intent();
+        int code = Integer.parseInt(enteredCode);
+        intent.putExtra("placeholder", enteredCode);
+
+
+    }
+
+    public void openMedals(View view)
+    {
         Intent openMedalsIntent = new Intent(this, MedalActivity.class);
         startActivity(openMedalsIntent);
     }
