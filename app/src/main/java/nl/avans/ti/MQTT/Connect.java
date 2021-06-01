cdf2d318a9dd2a20a52639334d4f2746c75407c0
yassindiriye <y.diriye@student.avans.nl>
2021-06-01 11:08:59 +0200

    made connection.java
    made a class to connect to the MQTT Broker

---
+++ app/src/main/java/nl/avans/ti/MQTT/Connect.java
@@ -0,0 +1,189 @@
+package nl.avans.ti.MQTT;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+import nl.avans.ti.MainActivity;
+
+public class Connect {
+    private MainActivity app;
+    private static final String LOGTAG = MainActivity.class.getName();
+
+    //broker settings
+    private static final String BROKER_HOST_URL = "tcp://sendlab.nl:11884";
+    private static final String USERNAME = "ti";
+    private static final String PASSWORD = "tiavans";
+
+    //client ID for broker
+    private static final String CLIENT_ID = "A3_APP_" + UUID.randomUUID().toString();
+    private static final int QUALITY_OF_SERVICE = 0;
+
+    //MQTT subscription topics
+    private static final String test = "ti/1.4/a3/test";
+
+    private MqttAndroidClient mqttAndroidClient;
+
+    public Connect(MainActivity app) {
+        this.app = app;
+        this.mqttAndroidClient = new MqttAndroidClient(app.getApplicationContext(), BROKER_HOST_URL, CLIENT_ID);
+
+        this.mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.d(LOGTAG, "MQTT client lost connection to broker, cause: " + cause.getLocalizedMessage());
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d(LOGTAG, "MQTT client delivery complete");
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        connectToBroker(this.mqttAndroidClient, CLIENT_ID);
+    }
+
+
+    private void connectToBroker(MqttAndroidClient client, String client_id){
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(false);
+        options.setUserName(USERNAME);
+        options.setPassword(PASSWORD.toCharArray());
+
+        try {
+            // Try to connect to the MQTT broker
+            IMqttToken token = client.connect(options);
+            // Set up callbacks for the result
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(LOGTAG, "MQTT client is now connected to MQTT broker");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOGTAG, "MQTT client failed to connect to MQTT broker: " +
+                            exception.getLocalizedMessage());
+                    Toast toast = Toast.makeText(app.getApplicationContext(),
+                            "Failed to connect to MQTT broker", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(LOGTAG, "MQTT exception while connecting to MQTT broker, reason: " +
+                    e.getReasonCode() + ", msg: " + e.getMessage() + ", cause: " + e.getCause());
+            e.printStackTrace();
+        }
+
+    }
+
+    private void disconnectFromBroker(MqttAndroidClient client){
+        try {
+            // Try to disconnect from the MQTT broker
+            IMqttToken token = client.disconnect();
+            // Set up callbacks to handle the result
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(LOGTAG, "MQTT client is now disconnected from MQTT broker");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOGTAG, "MQTT failed to disconnect from MQTT broker: " +
+                            exception.getLocalizedMessage());
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(LOGTAG, "MQTT exception while disconnecting from MQTT broker, reason: " +
+                    e.getReasonCode() + ", msg: " + e.getMessage() + ", cause: " + e.getCause());
+            e.printStackTrace();
+        }
+    }
+
+    private void publishMessage(MqttAndroidClient client, String topic, String msg) {
+        byte[] encodedPayload = new byte[0];
+        try {
+            // Convert the message to a UTF-8 encoded byte array
+            encodedPayload = msg.getBytes("UTF-8");
+            // Store it in an MqttMessage
+            MqttMessage message = new MqttMessage(encodedPayload);
+            // Set parameters for the message
+            message.setQos(QUALITY_OF_SERVICE);
+            message.setRetained(false);
+            // Publish the message via the MQTT broker
+            client.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            Log.e(LOGTAG, "MQTT exception while publishing topic to MQTT broker, msg: " + e.getMessage() +
+                    ", cause: " + e.getCause());
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribeToTopic(MqttAndroidClient client, final String topic) {
+        try {
+            // Try to subscribe to the topic
+            IMqttToken token = client.subscribe(topic, QUALITY_OF_SERVICE);
+            // Set up callbacks to handle the result
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(LOGTAG, "MQTT client is now subscribed to topic " + topic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOGTAG, "MQTT failed to subscribe to topic " + topic + " because: " +
+                            exception.getLocalizedMessage());
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(LOGTAG, "MQTT exception while subscribing to topic on MQTT broker, reason: " +
+                    e.getReasonCode() + ", msg: " + e.getMessage() + ", cause: " + e.getCause());
+            e.printStackTrace();
+        }
+    }
+
+    private void unsubscribeToTopic(MqttAndroidClient client, final String topic) {
+        try {
+            // Try to unsubscribe to the topic
+            IMqttToken token = client.unsubscribe(topic);
+            // Set up callbacks to handle the result
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(LOGTAG, "MQTT client is now unsubscribed to topic " + topic);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(LOGTAG, "MQTT client failed to unsubscribe to topic " + topic + " because: " +
+                            exception.getLocalizedMessage());
+                }
+            });
+        } catch (MqttException e) {
+            Log.e(LOGTAG, "MQTT exception while unsubscribing from topic on MQTT broker, reason: " +
+                    e.getReasonCode() + ", msg: " + e.getMessage() + ", cause: " + e.getCause());
+            e.printStackTrace();
+        }
+    }
+}
+
--- app/src/main/java/nl/avans/ti/MainActivity.java
+++ app/src/main/java/nl/avans/ti/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import nl.avans.ti.MQTT.Connect;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Connect connection = new Connect(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
