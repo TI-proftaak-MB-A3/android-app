5ef839a25266c1c7d3be2595005d650dd7e694d0
Twan van Noorloos <twan.v.n@outlook.com>
2021-06-03 16:15:59 +0200

    Added help page layout

--- app/src/main/java/nl/avans/ti/HelpActivity.java
+++ app/src/main/java/nl/avans/ti/HelpActivity.java
@@ -2,13 +2,21 @@ package nl.avans.ti;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelpActivity extends AppCompatActivity
 {
 
+    private ViewPager pager;
+    private PagerAdapter adapter;
 
     private MenuHandler menuHandler;
 
@@ -19,7 +27,16 @@ public class HelpActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        List<Fragment> list = new ArrayList<>();
+        list.add(new FragmentOne());
+        list.add(new FragmentTwo());
+        list.add(new FragmentThree());
+        list.add(new FragmentFour());
+        list.add(new FragmentFive());
 
+        this.pager = findViewById(R.id.pager);
+        adapter = new Slider(getSupportFragmentManager(), list);
+        pager.setAdapter(adapter);
 
 
         menuHandler = new MenuHandler(this);
